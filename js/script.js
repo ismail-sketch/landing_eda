710579e9635a690dda855a8a76e9d87126633e08
gennadiy <gennadiy.mutalibov@gmail.com>
2020-11-16 14:17:29 +0300

    модальное окно

--- js/script.js
+++ js/script.js
@@ -98,5 +98,59 @@ function getTimeRemaining(endtime) {
         }
     }    
     setClock('.timer', deadline);
+//============================================================================================
+    // МОДАЛЬНОЕ ОКНО УРОК 43
+
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+          modal = document.querySelector('.modal'),
+          modalCloseBtn = document.querySelector('[data-close]');
+          
+    function openModal() {
+        // modal.classList.add('show');
+        // modal.classList.remove('hide');
+        modal.classList.toggle('show');
+        document.body.style.overflow = 'hidden';
+        clearInterval(modalTimeId);
+     }
+    
+    modalTrigger.forEach(btn => {
+        btn.addEventListener('click',  openModal);
+    });
+    
+   
+    
+    function closeModal() {
+        // modal.classList.add('hide');
+        // modal.classList.remove('show');
+        modal.classList.toggle('show');
+        document.body.style.overflow = '';
+    }
+
+    modalCloseBtn.addEventListener('click', closeModal);
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {  
+           closeModal();
+       }
+   });
+
+   document.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape' && modal.classList.contains('show')) {
+            closeModal();
+        }
+   });
+
+   // Всплытие модального окна через опрделенное время
+
+   const modalTimeId = setTimeout(openModal, 3000);
+
+   function showModalByScroll() {
+    if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight ) {
+        openModal();
+        window.removeEventListener('scroll', showModalByScroll);
+    }
+   }
+   
+   window.addEventListener('scroll', showModalByScroll);
     
  }); // конец домконтент
